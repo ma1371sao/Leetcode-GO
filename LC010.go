800188da0655c927f721d10a676f0e483e402871
Shijie Ma <ma.1371sao@gmail.com>
2020-07-07 17:28:25 -0700

    add LC010.go

---
+++ LC010.go
@@ -0,0 +1,27 @@
+func isMatch(s string, p string) bool {
+    dp := make([][]bool, len(s) + 1)
+    for i := range dp {
+        dp[i] = make([]bool, len(p) + 1)
+    }
+    dp[0][0] = true
+    for j := 0; j < len(p); j++ {
+        if p[j] == '*' {
+            dp[0][j + 1] = dp[0][j - 1]
+        }
+    }
+    
+    for i := 0; i < len(s); i++ {
+        for j := 0; j < len(p); j++ {
+            if s[i] == p[j] || p[j] == '.' {
+                dp[i + 1][j + 1] = dp[i][j]
+            } else if p[j] == '*' {
+                if s[i] == p[j - 1] || p[j - 1] == '.' {
+                    dp[i + 1][j + 1] = (dp[i][j + 1] || dp[i][j - 1] || dp[i + 1][j - 1])
+                } else {
+                    dp[i + 1][j + 1] = dp[i + 1][j - 1]
+                }
+            }
+        }
+    }
+    return dp[len(s)][len(p)]
+}
